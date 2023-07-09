8ddab1addfd2d4211c78e4b8f849aa230fc33df4
Rajat Agrawal <rajat.visitme@gmail.com>
2023-07-09 23:58:01 +0530

    Adding models (Gen + Disc)
    Generator and Discriminator models architecture

---
+++ models.py
@@ -0,0 +1,66 @@
+# Random weights initializer
+init_weights_kernel = tf.keras.initializers.RandomNormal(mean = 0.0, stddev = 0.02)
+
+# Generator model architecture
+def generator_model():
+    model = tf.keras.Sequential()
+
+    model.add(layers.Input(shape = (100, )))
+    model.add(layers.Dense(16 * 16 * 256))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+    model.add(layers.Reshape((16, 16, 256)))
+
+    model.add(layers.Conv2DTranspose(128, kernel_size = 4, padding = 'same', strides = 2,
+                                     kernel_initializer = init_weights_kernel , use_bias = False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+
+    model.add(layers.Conv2DTranspose(64, kernel_size = 4, padding = 'same', strides = 2,
+                                     kernel_initializer = init_weights_kernel, use_bias = False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+
+    model.add(layers.Conv2DTranspose(32, kernel_size = 4, padding = 'same', strides = 2,
+                                     kernel_initializer = init_weights_kernel, use_bias = False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.ReLU())
+
+    model.add(layers.Conv2DTranspose(3, kernel_size = 4, padding = 'same', strides = 2, activation = 'tanh',
+                                     kernel_initializer = init_weights_kernel, use_bias = False))
+
+
+    return model
+
+# Discriminator model architecture
+def discriminator_model():
+    model = tf.keras.Sequential()
+
+    model.add(layers.Conv2D(32, kernel_size = 3, strides = 2, input_shape = (256, 256, 3), padding = 'same',
+                            kernel_initializer = init_weights_kernel, use_bias = False ))
+    model.add(layers.LeakyReLU(alpha = 0.2))
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(64, kernel_size = 3, strides = 2, padding = 'same',
+                            kernel_initializer = init_weights_kernel, use_bias = False))
+    model.add(layers.ZeroPadding2D(padding = ((0, 1), (0, 1))))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha = 0.2))
+    model.add(layers.Dropout(0.25))
+
+    model.add(layers.Conv2D(128, kernel_size = 3, strides = 2, padding = 'same',
+                            kernel_initializer = init_weights_kernel, use_bias = False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha = 0.2))
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(256, kernel_size = 3, strides=1, padding = 'same',
+                            kernel_initializer = init_weights_kernel, use_bias = False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha = 0.2))
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1, activation = 'sigmoid'))
+
+    return model
