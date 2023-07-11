01f370fa9919d3014e3f8d382ecefb5c3262d372
Rajat Agrawal <rajat.visitme@gmail.com>
2023-07-11 23:52:20 +0530

    Update main.py
    Complete  implementation

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# Libraries for data preprocessing
+# Import all required libraries
 import os
 import time
-import numpy as np
-import pandas as pd
-import random
 
-# importing cv2 for image reading and image processing
-import cv2
-from google.colab.patches import cv2_imshow
+# For data visualization
+import matplotlib.pyplot as plt
 
 # tensorflow - to implement neural network
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
-from tensorflow.keras import layers
+from train import train
 
 from models import generator_model, discriminator_model
+
+## DATASET
+# Dataset link: 
+
+
 ## CREATE TRAINING DATA
 
 # Defining the path to dataset directory
@@ -42,17 +42,18 @@ dataset = dataset.map(preprocess_image)
 # Create training batches
 dataset = dataset.batch(batch_size)
 
+
+## MODELS
+
 ## Create generator model
 generator = generator_model()
-
 ## Get model summary
-# generator.summary()
-
+generator.summary()
 ## Create discriminator model
 discriminator = discriminator_model()
-
 # # Get model summary
-# discriminator.summary()
+discriminator.summary()
+
 
 ## HYPERPARAMETERS
 
@@ -91,3 +92,93 @@ def generator_loss(fake_output):
     gen_loss = bce(tf.ones_like(fake_output), fake_output)
 
     return gen_loss
+
+
+## OPTIMIZER
+    
+# Initializing the optimizers
+generator_optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0002, beta_1 = 0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0002, beta_1 = 0.5)
+
+## CHECKPOINTS
+
+# Creating checkpoints
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt_n')
+checkpoint = tf.train.Checkpoint(generator_optimizer = generator_optimizer,
+                                 discriminator_optimizer = discriminator_optimizer,
+                                 generator = generator,
+                                 discriminator = discriminator)
+
+## Other Parameters
+
+# Hyperparameters
+epochs = 1000
+noise_dim = 100
+num_examples_to_generate = 16
+
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+# Helper function to display training images
+def generate_and_plot_images(model, epoch, test_input):
+
+    predictions = model(test_input, training = False)
+
+    fig = plt.figure(figsize = (8, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow((predictions[i, :, :, :] * 0.5 + 0.5))
+        plt.axis('off')
+
+    plt.savefig('/<dir path to save training results>/image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
+
+## MODEL TRAINING
+
+# Call the train function to train the models
+gen_loss_epochs, disc_loss_epochs, real_score_list, fake_score_list = train(dataset, epochs = epochs)
+
+## RESULTS
+
+# Loss and Accuracy Plots
+fig, (ax1,ax2) = plt.subplots(1, 2, figsize = (12, 8))
+
+ax1.plot(gen_loss_epochs, label = 'Generator loss', alpha = 0.5)
+ax1.plot(disc_loss_epochs, label = 'Discriminator loss', alpha = 0.5)
+ax1.legend()
+ax1.set_title('Training Losses')
+
+ax2.plot(real_score_list, label = 'Real_score', alpha = 0.5)
+ax2.plot(fake_score_list, label = 'Fake_score', alpha = 0.5)
+ax2.set_title('Accuracy Scores')
+ax2.legend()
+
+## SAVE MODELS
+
+# Save the entire model to a HDF5 file.
+# The '.h5' extension indicates that the model should be saved to HDF5.
+generator.save('/content/PokeGAN_1_dot_0_generator.h5')
+discriminator.save('/content/PokeGAN_1_dot_0_discriminator.h5')
+
+## TEST
+
+# Recreate the exact same model, including its weights and the optimizer
+trained_model = tf.keras.models.load_model('/content/PokeGAN_1_dot_0_generator.h5')
+
+# Show the model architecture
+trained_model.summary()
+
+# Create some random noise for test input
+test_input = tf.random.normal([num_examples_to_generate, noise_dim])
+
+# Generate new Pokemons
+predictions = trained_model(test_input, training = False)
+
+fig = plt.figure(figsize = (8, 4))
+
+for i in range(predictions.shape[0]):
+    plt.subplot(4, 4, i + 1)
+    plt.imshow((predictions[i, :, :, :]))
+    plt.axis('off')
+plt.show()
